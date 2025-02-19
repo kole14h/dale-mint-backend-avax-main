ec2425029396e84e5b1296a7b9e0f01b0c4608e5
kole14h <37876787+kole14h@users.noreply.github.com>
2025-02-19 10:25:06 +0000

    Update coin_flip.ts

--- tests/coin_flip.ts
+++ tests/coin_flip.ts
@@ -3,7 +3,6 @@ import { Program } from "@project-serum/anchor";
 import { CoinFlip } from "../target/types/coin_flip";
 
 describe("coin_flip", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.CoinFlip as Program<CoinFlip>;
